96fad405194b363abc27b59786e5b642ddaa3b2f
Howard Stearns <howard.stearns@gmail.com>
2026-07-06 10:47:18 -0700

    missing

---
+++ common.js
@@ -0,0 +1,4 @@
+export const styles = ["🎙️ news", "🎸 classic rock", "🎷 jazz", "🎵 pop", "🎤 hiphop", "🎧 edm", "🤠 country", "🎼 classical"];
+// techo, house, public radio, top40, 50's, 60's, etc.
+
+export const streamingRootPath = "./streaming-radio";
